e6b2992c5c76066c31904975e80ea803bb189ca3
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-04 15:22:24 +0530

    DSA

---
+++ sumoddele.scala
@@ -0,0 +1,9 @@
+def f(arr: List[Int]): Int = {
+  def sum(arr: List[Int], total: Int = 0): Int = {
+    if (arr.isEmpty) return total
+    if (arr.head % 2 != 0) return sum(arr.tail, arr.head + total) 
+    else return sum(arr.tail, total)
+  }
+
+  sum(arr)
+}
