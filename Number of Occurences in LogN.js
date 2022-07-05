ea6db0d2b5a92c1cb984fe4859171e26e2b7a47f
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-05 17:44:38 +0530

    Update Number of Occurences in LogN.js

--- Number of Occurences in LogN.js
+++ Number of Occurences in LogN.js
@@ -1,49 +1,25 @@
 // You are given n different numbers and an integer k. Write a program that finds number of times k is present in log(n) time complexity.
-
 function runProgram(input){
-    var input=input.split("\n");
-    var size=input[0].split(" ").map(Number);
-    var n=size[0];
-    var k=size[1];
-    var arr=input[1].split(" ").map(Number);
-    solve(n,k,arr);
+    input=input.trim().split("\n");
     
-}
-function solve(n,k,arr){
-    var first=firstOrlast(arr,k,true);
-    var last=firstOrlast(arr,k,false);
-    console.log((last-first)+1)
-    
-}
-
-
-function firstOrlast(arr,k,bool){
-    let low=0;
-    let high=arr.length-1;
-    let result=0;
-    while(low<=high){
-        var mid= Math.floor(low+(high-low)/2);
-        if(arr[mid]==k){
-            if(bool){
-                result=mid;
-                high=mid-1;
-            }
-            else{
-                result=mid;
-                low=mid+1;
-            }
-        }
-        else if(arr[mid]<k){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
+    var [n,k]=input[0].trim().split(" ")
+    var arr=input[1].trim().split(" ").map(Number)
+    console.log(f(arr,k));
     }
-    return result;
+
+function f(arr,k){
+    var n = 0;
+    for(i = 0; i < arr.length; i++){
+        if(arr[i] == k){n++}
+    }
+    return n;
 }
 
- 
+
+
+
+
+
 if (process.env.USER === "") {
   runProgram(``);
 } else {
