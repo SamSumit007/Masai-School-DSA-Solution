80f7673f2f492f67a315c67a6fafdbc22a08bf95
Sumit Thakur <97454993+SamSumit007@users.noreply.github.com>
2022-07-11 18:10:17 +0530

    Update Power of 2.js

--- Power of 2.js
+++ Power of 2.js
@@ -1,19 +1,45 @@
 //Given an integer n, return true if it is a power of two. Otherwise, return false.
 //An integer n is a power of two, if there exists an integer x such that n == 2x.
 
-var isPowerOfTwo = function (n) {
-    var flag = false;
-    for (var i = 0; i < 31; i++) {
-        if ((2 ** i) == n) {
-            flag = true;
-            break;
-        }
-    }
-    if (flag) {
-        return true;
-    }
-    else {
-        return false;
-    }
+ function Square(n)  {
+   if(n===1){
+       return "True";
+   } 
+   else if (n%2!==0 || n===0){
+       return "False";
+   }
+   return Square(n/2); 
+ } 
+    
+function runProgram(input){
+    input=input.trim().split("\n");
+     var T=+input[0];
+    var line = 1;
+    
+   for(var i=0; i<T; i++){
+     
+      var n=+input[line++];
+      console.log(Square(n));
+   }  
+}   
 
-};
+if (process.env.USER === "") {
+  runProgram(``);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
